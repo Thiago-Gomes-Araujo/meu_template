079ff706949d8cc7044f071ee888e53d0b864462
Thiago-Gomes <thiagogomes543@hotmail.com>
2024-10-25 01:56:08 -0400

    criando avalicao

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  
+  include Pagy::Backend
+  layout :set_layout
+
+  def set_layout
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+  # Captura a exceção AccessDenied do CanCanCan
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome, :telefone])
+  end
 end
